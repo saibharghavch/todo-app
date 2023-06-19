aa505d56a8fac0b124c44a54606a3523502d2ec2
saibharghav <64954492+saibharghav@users.noreply.github.com>
2023-06-19 11:26:56 -0400

    Create basic GUI without interactions

--- cli.py
+++ cli.py
@@ -3,6 +3,7 @@ import time
 
 now = time.strftime('%b %d,%A %Y %H:%M:%S')
 print("It is ",now)
+
 while True:
     user_action = input('Type add, show, edit, complete, or exit: ')
     if user_action.startswith('add'):
