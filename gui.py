ee9488e1e2205cec980b3a9517270dd8b0304a65
saibharghav <64954492+saibharghav@users.noreply.github.com>
2023-06-21 08:35:54 -0400

    Change theme, add clock, handle edit and complete errors

--- gui.py
+++ gui.py
@@ -1,5 +1,10 @@
 import functions
 import PySimpleGUI as sg
+import time
+
+sg.theme("Black")
+
+clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key='todo')
 add_button = sg.Button("Add")
@@ -9,15 +14,16 @@ complete_button=sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
 window=sg.Window('My To-Do App',
-                 layout=[[label],
+                 layout=[[clock],
+                         [label],
                          [input_box,add_button],
                          [list_box,edit_button, complete_button],
                          [exit_button]],
                  font=('Helvetica', 18))
 while True:
-    event,values = window.read()
-    print(event)
-    print(values)
+    event,values = window.read(timeout=200)
+    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -25,28 +31,34 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)
         case "Edit":
-            # By clicking on the buttons there is a dictionary which is created with events and values
-            # To get the to-do which needed to be edited
-            todo_to_edit = values['todos'][0]
-            # We write the to-do which needs to be inplace of the to-do select
-            new_todo = values['todo']
-            todos = functions.get_todos()
-            # Get the index and then override it
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            # Once the update is done on the backend we need to update it on the GUI so we use update function
-            # window is like the parent object which contains all the layouts
-            window['todos'].update(values=todos)
+            try:
+                # By clicking on the buttons there is a dictionary which is created with events and values
+                # To get the to-do which needed to be edited
+                todo_to_edit = values['todos'][0]
+                # We write the to-do which needs to be inplace of the to-do select
+                new_todo = values['todo']
+                todos = functions.get_todos()
+                # Get the index and then override it
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                # Once the update is done on the backend we need to update it on the GUI so we use update function
+                # window is like the parent object which contains all the layouts
+                window['todos'].update(values=todos)
+            except IndexError:
+                sg.popup("Please select an item first", font=("Helvetica", 20))
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case 'Complete':
-            todo_to_complete=values['todos'][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
+            try:
+                todo_to_complete=values['todos'][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Please select an item first", font=("Helvetica", 20))
         case "Exit":
             break
         case sg.WIN_CLOSED:
