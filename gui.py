7e8da5e4a601140799e5811cdc6695016e736b0e
saibharghav <64954492+saibharghav@users.noreply.github.com>
2023-06-22 09:07:26 -0400

    Create a static web interface

--- gui.py
+++ gui.py
@@ -1,13 +1,19 @@
 import functions
 import PySimpleGUI as sg
 import time
+import os
+
+if not os.path.exists("todos.txt"):
+    with open('todos.txt','w') as file:
+        pass
 
 sg.theme("Black")
 
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key='todo')
-add_button = sg.Button("Add")
+add_button = sg.Button(size=10, image_source="add.png",
+                       tooltip="Add Todo", key="Add")
 list_box = sg.Listbox(values=functions.get_todos(),key='todos', enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
 complete_button=sg.Button("Complete")
@@ -21,6 +27,8 @@ window=sg.Window('My To-Do App',
                          [exit_button]],
                  font=('Helvetica', 18))
 while True:
+    # For getting the time displayed and changed every second we do this line of code so that after every 200 milliseconds
+    # the clock will update itself
     event,values = window.read(timeout=200)
     window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
 
